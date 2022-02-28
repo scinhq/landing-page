621e46c919a642f2b9c93a19b30fa192b5d572e3
Amirsalar Heydari <amirsalar@scin.io>
2022-02-27 21:56:13 -0800

    Debug SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Script from 'next/script';
-import SEO from '../components/Seo';
+import SEO from '../components/seo';
 
 export default function Home() {
   return (
