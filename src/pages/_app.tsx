e4c0055cc9617c4663b3abab0c9f7abaec0bbae1
Amirsalar Heydari <amirsalaar@gmail.com>
2022-04-01 08:08:12 -0700

    Restructure the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css';
-import '../styles/home/bootstrap-5.0.0-alpha-2.min.css';
-import '../styles/home/LineIcons.2.0.css';
-import '../styles/home/animate.css';
-import '../styles/home/main.css';
+import '../assets/styles/globals.css';
+import '../assets/styles/home/bootstrap-5.0.0-alpha-2.min.css';
+import '../assets/styles/home/LineIcons.2.0.css';
+import '../assets/styles/home/animate.css';
+import '../assets/styles/home/main.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
