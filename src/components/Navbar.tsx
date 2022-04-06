c3a8055dd2f58fa8114846771877b502d109e146
Amirsalar Heydari <amirsalaar@gmail.com>
2022-04-06 01:03:28 -0700

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,55 +5,51 @@ import Logo from '../../public/img/logo/logo.png';
 export default function Navbar() {
   return (
     <>
-      {/* <!-- ========================= header start ========================= --> */}
-      <header className="header">
-        <div className="navbar-area">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-lg-12">
-                <nav className="navbar fixed-top navbar-expand-lg bg-white">
-                  <div className="container">
-                    <Link href="./" passHref>
-                      <a className="navbar-brand">
-                        <div style={{ width: '12rem' }}>
-                          <Image
-                            className="logo-1"
-                            src={Logo}
-                            alt="SCIN Logo"
-                            layout="responsive"
-                            width={1000}
-                            height={400}
-                          />
-                        </div>
-                      </a>
-                    </Link>
-                    {/* left adjusted links */}
-                    <div className="" id="navbarSupportedContent">
-                      <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                          <Link href="./" passHref>
-                            <a className="nav-link">Home</a>
-                          </Link>
-                        </li>
-                        <li className="nav-item">
-                          <Link href="/team" passHref>
-                            <a className="nav-link">Team</a>
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </nav>
-                {/* <!-- navbar --> */}
+      <nav className="navbar fixed-top navbar-expand-lg bg-white">
+        <div className="container">
+          <Link href="./" passHref>
+            <a className="navbar-brand">
+              <div style={{ width: '12rem' }}>
+                <Image
+                  className="logo-1"
+                  src={Logo}
+                  alt="SCIN Logo"
+                  layout="responsive"
+                  width={1000}
+                  height={400}
+                />
               </div>
-            </div>
-            {/* <!-- row --> */}
+            </a>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link href="./" passHref>
+                  <a className="nav-link">Home</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/team" passHref>
+                  <a className="nav-link">Team</a>
+                </Link>
+              </li>
+            </ul>
           </div>
-          {/* <!-- container --> */}
         </div>
-        {/* <!-- navbar area --> */}
-      </header>
-      {/* <!-- ========================= header end ========================= --> */}
+      </nav>
+      {/* <!-- navbar --> */}
     </>
   );
 }
