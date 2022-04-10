73f1d61048a5f8d90a7c9482482de353178241af
Amirsalar Heydari <amirsalaar@gmail.com>
2022-04-10 13:52:44 -0700

    PLAT-5 Delete unused footer info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Logo from '../../public/img/logo/logo.svg';
 import { SocialButton } from './SocialButtons';
 
@@ -55,73 +56,15 @@ export default function FooterView() {
             </div>
           </div>
         </div>
-        <div className="col-6 col-md">
-          <h5>Features</h5>
-          <ul className="list-unstyled text-small">
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Cool stuff
-              </a>
-            </li>
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Random feature
-              </a>
-            </li>
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Team feature
-              </a>
-            </li>
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Stuff for developers
-              </a>
-            </li>
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Another one
-              </a>
-            </li>
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Last time
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="col-6 col-md">
-          <h5>Resources</h5>
-          <ul className="list-unstyled text-small">
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Resource
-              </a>
-            </li>
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Resource name
-              </a>
-            </li>
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Another resource
-              </a>
-            </li>
-            <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Final resource
-              </a>
-            </li>
-          </ul>
-        </div>
+        <div className="col-6 col-md"></div>
+        <div className="col-6 col-md"></div>
         <div className="col-6 col-md">
           <h5>About</h5>
           <ul className="list-unstyled text-small">
             <li className="mb-1">
-              <a className="link-secondary text-decoration-none" href="#">
-                Team
-              </a>
+              <Link href="./team" passHref>
+                <a className="link-secondary text-decoration-none">Team</a>
+              </Link>
             </li>
             <li className="mb-1">
               <a className="link-secondary text-decoration-none" href="#">
